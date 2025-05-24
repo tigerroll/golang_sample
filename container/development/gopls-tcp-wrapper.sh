f7e0f00273dc0db263105ee27b29f4b7556cac72
Your Name <you@example.com>
2025-05-24 21:40:17 +0900

    enhance: update container settings.

--- container/development/gopls-tcp-wrapper.sh
+++ container/development/gopls-tcp-wrapper.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+GOPLS_PORT=${GOPLS_PORT:-4000}
 GOPLS_PATH=$(goenv which gopls 2>/dev/null || command -v gopls 2>/dev/null || echo "gopls")
 echo "gopls TCP wrapper: Attempting to use gopls at: $GOPLS_PATH" >&2
 [[ $(which "${GOPLS_PATH}" 2>/dev/null) ]] || {
   echo "gopls TCP wrapper: gopls command not found into PATH." >&2
   exit 1
+} && {
+  echo "gopls TCP wrapper: Attempting to use gopls at: $(which gopls)"
+  echo "Starting gopls in TCP listen mode on port ${GOPLS_PORT}..."
+  gopls -listen=:${GOPLS_PORT} 2>&1 | tee /var/log/gopls.log &
+  GOPLS_PID=$!
+  echo "gopls started."
+  wait $GOPLS_PID
+  echo "gopls process (PID ${GOPLS_PID}) terminated."
 }
-
-exec socat TCP-LISTEN:4000,reuseaddr,fork EXEC:"$GOPLS_PATH"
