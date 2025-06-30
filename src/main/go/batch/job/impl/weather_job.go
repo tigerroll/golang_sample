49cfecaff31525dace6ec1a1f61f3d9d1d8fda42
Your Name <you@example.com>
2025-06-30 21:04:20 +0900

    remove unnecessary file.

--- src/main/go/batch/job/impl/weather_job.go
+++
@@ -1,5 +0,0 @@
-package impl
-
-// This file is deprecated and should ideally be removed.
-// Its functionality has been moved to src/main/go/batch/weather/job/weather_job.go.
-// If this file must exist for build purposes, it should remain empty or contain only comments.
